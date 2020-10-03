a12a070e541d986755e04bdbf59d9ff0d1d2f365
Reisan03 <syousei-pc@outlook.jp>
2020-10-03 16:16:04 +0900

    終わってなかった

---
+++ Section 7/bmiclass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace bmiclass
+{
+    class BMI
+    {
+        private double blm;
+
+        public double Calc(double bl, double bw)
+        {
+            blm = bl / 100.0;
+            return bw / Math.Pow(blm, 2.0);
+        }
+    }
+
+    class BMIclass
+    {
+        public static void Main()
+        {
+            string strBl, strBw;
+            double blcm, bwkg;
+
+            Console.Write("--- 身長(cm)");
+            strBl = Console.ReadLine();
+            blcm = double.Parse(strBl);
+
+            Console.Write("--- 体重(kg)");
+            strBw = Console.ReadLine();
+            bwkg = double.Parse(strBw);
+
+            BMI bmi = new BMI();
+
+            Console.WriteLine(bmi.Calc(blcm, bwkg));
+        }
+    }
+}
---
+++ Section 7/noreturnvalue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace noreturnvalue.cs
+{
+    class kakeibo
+    {
+        private int total = 0;
+
+        public void Nyukin(int en)
+        {
+            total += en;
+            Console.WriteLine("{0}円を入金しました", en);
+            return;
+        }
+
+        public void shishutu(int en)
+        {
+            if (total < en)
+            {
+                Console.WriteLine("残高が足らん 残高:{0}", total);
+            }
+            else
+            {
+                total -= en;
+                Console.WriteLine("{0}円支出しといた", en);
+                return;
+            }
+        }
+
+        public void GetTotal()
+        {
+            Console.WriteLine("{0}円なんやで",total);
+        }
+    }
+    class NoReturnValue
+    {
+        public static void Main()
+        {
+            kakeibo k = new kakeibo();
+
+            k.GetTotal();
+            k.Nyukin(10000);
+            k.shishutu(9000);
+            k.shishutu(6000);
+            k.GetTotal();
+        }
+    }
+}
