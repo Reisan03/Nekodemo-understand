9470c1b03e16b45938f12be0e8a041725b83a836
Reisan03 <syousei-pc@outlook.jp>
2020-10-03 12:17:30 +0900

    First

---
+++ Simpleclass/Simpleclass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Simpleclass
+{
+    class Myclass
+    {
+        public int x;
+    }
+
+    class SimpleClass
+    {
+        public static void Main()
+        {
+            Myclass mc = new Myclass();
+            mc.x = 10;
+            Console.WriteLine(mc.x);
+        }
+    }
+}
---
+++ simpleclass02/simpleclass02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace simpleclass02
+{
+    class Myclass
+    {
+        public int x; // インスタンス変数
+    }
+
+    class Simpleclass02
+    {
+        public static void Main()
+        {
+            Myclass a, b;
+            a = new Myclass(); // オブジェクト化
+            b = new Myclass(); // これもオブジェクト化
+
+            a.x = 10;
+            b.x = 100;
+
+            Console.WriteLine(a.x);
+            Console.WriteLine(b.x);
+        }
+    }
+}
